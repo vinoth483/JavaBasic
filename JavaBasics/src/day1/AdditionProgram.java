a941172afc0053080614c58118f81f73efaa78a3
vinoth483 <62651447+vinoth483@users.noreply.github.com>
2024-06-27 11:02:59 +0530

    This is my project

---
+++ JavaBasics/src/day1/AdditionProgram.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+		System.out.println("House of bikers");
+		System.out.println("Sparow");
+		System.out.print("s1000rr");
+		
+
+	}
+
+}
+	
